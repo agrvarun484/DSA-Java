bae8868bf3f3f02138414762772f0429db26db2e
agrvarun484 <100117027+agrvarun484@users.noreply.github.com>
2022-11-30 13:16:06 +0530

    Arrays

---
+++ array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class array {
+public static void main(String[] args) {
+    int []arr = new int[3];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    for(int i = 0; i < arr.length; i++){
+        System.out.print(arr[i]);
+        System.out.print(" ");
+    }
+}
+}
